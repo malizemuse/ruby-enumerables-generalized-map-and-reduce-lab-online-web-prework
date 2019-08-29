d7a553e13a161fa905b5b093e66660b4d1ae14ab
gregarious-number-3828 <muse.malize@gmail.com>
2019-08-29 14:06:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+# Method. Map.
+def map(array)
+  
+  new_array = []
+  i = 0 
+  
+  while i < array.length do
+    new_array << yield(array[i])
+    i += 1
+  end
+  
+  new_array
+  
+end
+
+
+# Method. Reduce.
+def reduce(elements, starting_point = nil, &block)
+  if starting_point.nil?
+    elements.reduce(&block)
+  else
+    elements.reduce(starting_point, &block)
+  end
+end
